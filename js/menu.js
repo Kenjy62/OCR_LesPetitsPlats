8c16897f2294385212572462d8be040f50e129f7
Nathan L <nathanlengagne@gmail.com>
2023-02-27 01:14:33 +0100

    Strongly Fix Listener

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 import { ingredients, appareils, ustensiles } from "./data.js";
-import { DOM } from "./dom.js";
+import { DOM, clearDOM } from "./dom.js";
+import { search, currentKeywords, currentTags } from "./search.js";
 
 // Make all menus items
 export function makeIngredients(list) {
@@ -79,4 +80,194 @@ DOM.ustensilesInput.addEventListener("focus", function (e) {
 
   let icon = DOM.ustensilesInput.nextElementSibling;
   icon.style.transform = "rotate(180deg)";
+});
+
+// Menu Search Listener When Write
+DOM.ingredientsInput.addEventListener("keydown", (e) =>
+  searchInTag(e, "ingredient")
+);
+
+// Clean Up Function
+function clearAllListerner() {
+  // Add Tag Clean Up
+  const items = document.querySelectorAll(
+    "#ingredients>span, #appareils>span, #ustensiles>span"
+  );
+  items.forEach((item) => item.removeEventListener("click", addTag));
+
+  // Delete Tag Clean Up
+  const tags = document.querySelectorAll("#tags>div>.deleteTags");
+  tags.forEach((item) => item.removeEventListener("click", deleteTag));
+}
+
+// Listener Builder
+function buildListerner(e) {
+  // Prevent Clear All Listener
+  clearAllListerner();
+
+  // For Adding Tag
+  const items = document.querySelectorAll(
+    "#ingredients>span, #appareils>span, #ustensiles>span"
+  );
+  items.forEach((item) => item.addEventListener("click", addTag));
+
+  // For Remove Tag
+  const tags = document.querySelectorAll("#tags>div>.deleteTags");
+  tags.forEach((item) => item.addEventListener("click", deleteTag));
+}
+
+// Function for Adding Tag
+const addTag = (e) => {
+  console.log("Adding Tag");
+  if (e.target.closest("#ingredients")) {
+    let obj = `<div data='${e.target.getAttribute(
+      "value"
+    )}' class="bg-customBlue rounded-sm py-2 px-3 text-white mr-2 text-sm flex items-center justify-center cursor-pointer">
+          <div>${e.target.textContent}</div>
+          <div class="deleteTags ml-2 h-5 w-5 border border-white border-solid rounded-full flex justify-center items-center text-xs">
+              <i class="fa-sharp fa-solid fa-xmark"></i>
+          </div>
+      </div>`;
+    DOM.tag.insertAdjacentHTML("beforeend", obj);
+    e.target.style.display = "none";
+    let tag = {
+      name: e.target.textContent,
+      type: "ingredient",
+      action: "Add",
+    };
+    updateTag(tag);
+  } else if (e.target.closest("#appareils")) {
+    let obj = `<div data='${e.target.getAttribute(
+      "value"
+    )}' class="bg-customGreen rounded-sm py-2 px-3 text-white mr-2 text-sm flex items-center justify-center cursor-pointer">
+          <div>${e.target.textContent}</div>
+          <div class="deleteTags ml-2 h-5 w-5 border border-white border-solid rounded-full flex justify-center items-center text-xs">
+              <i class="fa-sharp fa-solid fa-xmark"></i>
+          </div>
+      </div>`;
+    DOM.tag.insertAdjacentHTML("beforeend", obj);
+    e.target.style.display = "none";
+    let tag = {
+      name: e.target.textContent,
+      type: "appareil",
+      action: "Add",
+    };
+    updateTag(tag);
+  } else if (e.target.closest("#ustensiles")) {
+    let obj = `<div data='${e.target.getAttribute(
+      "value"
+    )}' class="bg-customOrange rounded-sm py-2 px-3 text-white mr-2 text-sm flex items-center justify-center cursor-pointer">
+          <div>${e.target.textContent}</div>
+          <div class="deleteTags ml-2 h-5 w-5 border border-white border-solid rounded-full flex justify-center items-center text-xs">
+              <i class="fa-sharp fa-solid fa-xmark"></i>
+          </div>
+      </div>`;
+    DOM.tag.insertAdjacentHTML("beforeend", obj);
+    e.target.style.display = "none";
+    let tag = {
+      name: e.target.textContent,
+      type: "ustensil",
+      action: "Add",
+    };
+    updateTag(tag);
+  }
+
+  buildListerner();
+  search(currentKeywords);
+};
+
+// Function for Delete Tag
+const deleteTag = (e) => {
+  let data = e.target.parentElement.parentElement.getAttribute("data");
+  e.target.parentElement.parentElement.remove();
+  let item = document.querySelectorAll(`span[value='${data}']`);
+  /// IM HERE
+  item[0].style.display = "block";
+  let tag = { name: data, type: "appareil", action: "Remove" };
+  updateTag(tag);
+  search(currentKeywords);
+};
+
+// Function Update Tag
+function updateTag(tag) {
+  if (tag.action === "Add") {
+    currentTags.push(tag);
+  } else if (tag.action === "Remove") {
+    let index = currentTags.findIndex((tag) => tag.name === tag.name);
+    currentTags.splice(index, 1);
+  }
+}
+
+// Search Input Tag Function
+function searchInTag(e, option) {
+  switch (option) {
+    case "ingredient":
+      if (e.currentTarget.value.length > 0) {
+        const results = ingredients.filter((ingredient) =>
+          ingredient.toLowerCase().includes(e.currentTarget.value.toLowerCase())
+        );
+        if (results.length > 0) {
+          clearDOM("ingredients");
+          makeIngredients(results);
+        } else {
+          clearDOM("ingredients");
+          DOM.ingredients.insertAdjacentHTML(
+            "beforeend",
+            `<span>Aucun résultat pour la recherche '${e.currentTarget.value}'...</span>`
+          );
+        }
+      } else {
+        clearDOM("ingredients");
+        makeIngredients(ingredients);
+      }
+      break;
+    case "appareil":
+      if (e.currentTarget.value.length > 0) {
+        const results = appareils.filter((appareils) =>
+          appareils.toLowerCase().includes(e.currentTarget.value.toLowerCase())
+        );
+        if (results.length > 0) {
+          clearDOM("appareils");
+          makeAppareils(results);
+        } else {
+          clearDOM("appareils");
+          DOM.appareils.insertAdjacentHTML(
+            "beforeend",
+            `<span>Aucun résultat pour la recherche '${e.currentTarget.value}'...</span>`
+          );
+        }
+      } else {
+        clearDOM("appareils");
+        makeAppareils(appareils);
+      }
+      break;
+    case "ustensil":
+      if (e.currentTarget.value.length > 0) {
+        const results = ustensiles.filter((ustensile) =>
+          ustensile.toLowerCase().includes(e.currentTarget.value.toLowerCase())
+        );
+        if (results.length > 0) {
+          clearDOM("ustensiles");
+          makeUstensiles(results);
+        } else {
+          clearDOM("ustensiles");
+          DOM.ustensiles.insertAdjacentHTML(
+            "beforeend",
+            `<span>Aucun résultat pour la recherche '${e.currentTarget.value}'...</span>`
+          );
+        }
+      } else {
+        clearDOM("ustensiles");
+        makeUstensiles(ustensiles);
+      }
+      break;
+    default:
+      break;
+  }
+  buildListerner();
+}
+
+// When DOM is Fully Load Create Listener
+document.addEventListener("DOMContentLoaded", function () {
+  buildListerner();
 });
--- js/search.js
+++ js/search.js
@@ -1,13 +1,13 @@
 // Required
 
 import { DOM, clearDOM, makeRecette, makeErrorMsg } from "./dom.js";
-import { recettes, ingredients, ustensiles, appareils } from "./data.js";
-import { makeAppareils, makeIngredients, makeUstensiles } from "./menu.js";
+import { recettes } from "./data.js";
 
 // Actually Search
-var currentKeywords;
-var currentTags = [];
-var currentResults;
+export var currentKeywords;
+export var currentTags = [];
+var currentResults = [];
+var currentResultsWithTags = [];
 
 // Searchbar Listerner
 DOM.searchbar.addEventListener("keyup", function (e) {
@@ -16,34 +16,41 @@ DOM.searchbar.addEventListener("keyup", function (e) {
 });
 
 // Search function
-function search(keywords) {
-  var result = [];
+export function search(currentKeywords) {
+  console.log(`Start Search with ${currentKeywords}`);
+
+  var results = [];
   var resultWithTags = [];
 
-  if (keywords != undefined && keywords.length > 2) {
+  if (currentKeywords != undefined && currentKeywords.length > 2) {
     for (let [index, value] of recettes.entries()) {
       if (
-        value.name.toLowerCase().includes(keywords.toLowerCase()) ||
-        value.description.toLowerCase().includes(keywords.toLowerCase())
+        value.name.toLowerCase().includes(currentKeywords.toLowerCase()) ||
+        value.description.toLowerCase().includes(currentKeywords.toLowerCase())
       ) {
-        result.push(value);
+        results.push(value);
       } else {
         for (let [i, ingredient] of recettes[index].ingredients.entries()) {
           if (
-            ingredient.ingredient.toLowerCase().includes(keywords.toLowerCase())
+            ingredient.ingredient
+              .toLowerCase()
+              .includes(currentKeywords.toLowerCase())
           ) {
-            result.push(recettes[index]);
+            results.push(recettes[index]);
           }
         }
       }
-    }
-  }
 
-  if (currentTags.length > 0) {
-    for (let tag of currentTags) {
-      switch (tag.type) {
-        case "ingredient":
-          for (let [index, recette] of result.entries()) {
+      currentResults = results;
+      console.log("Recettes trouvées");
+      console.log(currentResults);
+    }
+
+    if (currentTags.length > 0) {
+      console.log(currentTags);
+      for (let tag of currentTags) {
+        if (tag.type === "ingredient") {
+          for (let [index, recette] of currentResults.entries()) {
             for (let [i, ingredient] of recette.ingredients.entries()) {
               if (
                 ingredient.ingredient
@@ -54,200 +61,28 @@ function search(keywords) {
               }
             }
           }
-          break;
-        case "appareil":
-          for (let [index, recette] of result.entries()) {
+          currentResults = resultWithTags;
+          console.log("trouvée avec tags");
+          console.log(currentResults);
+        } else if (tag.type === "appareil") {
+          for (let [index, recette] of currentResults.entries()) {
             if (
               recette.appliance.toLowerCase().includes(tag.name.toLowerCase())
             ) {
+              resultWithTags = [];
               resultWithTags.push(recette);
             }
           }
-          break;
-        case "ustensil":
-          for (let [index, recette] of result.entries()) {
-            for (let [i, ustensil] of recette.ustensils.entries()) {
-              if (ustensil.toLowerCase().includes(tag.name.toLowerCase())) {
-                resultWithTags.push(recette);
-              }
-            }
-          }
-          break;
-        default:
-          break;
+        }
       }
     }
-  }
 
-  if (result.length > 0 || resultWithTags.length > 0) {
-    if (currentTags.length > 0) {
-      clearDOM("recettes");
-      clearDOM("errorMsg");
-      makeRecette(resultWithTags);
-    } else {
-      clearDOM("recettes");
-      clearDOM("errorMsg");
-      makeRecette(result);
+    if (resultWithTags.length > 0) {
+      currentResults = resultWithTags;
     }
+
+    clearDOM("recettes");
+    clearDOM("errorMsg");
+    makeRecette(currentResults);
   }
-}
-
-// Add // Remove tags
-function AddOrRemoveTag(obj) {
-  if (obj.action === "Add") {
-    currentTags.push({ name: obj.name, type: obj.type });
-    search(currentKeywords);
-  } else if (obj.action === "Remove") {
-    let index = currentTags.findIndex((tag) => tag.name === obj.name);
-    currentTags.splice(index, 1);
-    search(currentKeywords);
-  }
-}
-
-// Tags Listeners
-DOM.ingredientsInput.addEventListener("keyup", function (e) {
-  if (e.currentTarget.value.length > 0) {
-    const results = ingredients.filter((ingredient) =>
-      ingredient.toLowerCase().includes(e.currentTarget.value.toLowerCase())
-    );
-    if (results.length > 0) {
-      clearDOM("ingredients");
-      makeIngredients(results);
-    } else {
-      clearDOM("ingredients");
-      DOM.ingredients.insertAdjacentHTML(
-        "beforeend",
-        `<span>Aucun résultat pour la recherche '${e.currentTarget.value}'...</span>`
-      );
-    }
-  } else {
-    clearDOM("ingredients");
-    makeIngredients(ingredients);
-  }
-
-  addingListenerToItems();
-});
-
-DOM.appareilsInput.addEventListener("keyup", function (e) {
-  e.stopImmediatePropagation();
-  if (e.currentTarget.value.length > 0) {
-    const results = appareils.filter((appareils) =>
-      appareils.toLowerCase().includes(e.currentTarget.value.toLowerCase())
-    );
-    if (results.length > 0) {
-      clearDOM("appareils");
-      makeAppareils(results);
-    } else {
-      clearDOM("appareils");
-      DOM.appareils.insertAdjacentHTML(
-        "beforeend",
-        `<span>Aucun résultat pour la recherche '${e.currentTarget.value}'...</span>`
-      );
-    }
-  } else {
-    clearDOM("appareils");
-    makeAppareils(appareils);
-  }
-});
-
-DOM.ustensilesInput.addEventListener("keyup", function (e) {
-  if (e.currentTarget.value.length > 0) {
-    const results = ustensiles.filter((ustensile) =>
-      ustensile.toLowerCase().includes(e.currentTarget.value.toLowerCase())
-    );
-    if (results.length > 0) {
-      clearDOM("ustensiles");
-      makeUstensiles(results);
-    } else {
-      clearDOM("ustensiles");
-      DOM.ustensiles.insertAdjacentHTML(
-        "beforeend",
-        `<span>Aucun résultat pour la recherche '${e.currentTarget.value}'...</span>`
-      );
-    }
-  } else {
-    clearDOM("ustensiles");
-    makeUstensiles(ustensiles);
-  }
-});
-
-// Adding listener for item in menu 'before/after modification'
-function addingListenerToItems() {
-  const items = document.querySelectorAll(
-    "#ingredients>span, #appareils>span, #ustensiles>span"
-  );
-  items.forEach((item) => {
-    item.addEventListener("click", function (e) {
-      if (e.target.closest("#ingredients")) {
-        let obj = `<div data='${e.target.getAttribute(
-          "value"
-        )}' class="bg-customBlue rounded-sm py-2 px-3 text-white mr-2 text-sm flex items-center justify-center cursor-pointer">
-                <div>${e.target.textContent}</div>
-                <div class="deleteTags ml-2 h-5 w-5 border border-white border-solid rounded-full flex justify-center items-center text-xs">
-                    <i class="fa-sharp fa-solid fa-xmark"></i>
-                </div>
-            </div>`;
-        DOM.tag.insertAdjacentHTML("beforeend", obj);
-        e.target.style.display = "none";
-        let tag = {
-          name: e.target.textContent,
-          type: "ingredient",
-          action: "Add",
-        };
-        AddOrRemoveTag(tag);
-      } else if (e.target.closest("#appareils")) {
-        let obj = `<div data='${e.target.getAttribute(
-          "value"
-        )}' class="bg-customGreen rounded-sm py-2 px-3 text-white mr-2 text-sm flex items-center justify-center cursor-pointer">
-                <div>${e.target.textContent}</div>
-                <div class="deleteTags ml-2 h-5 w-5 border border-white border-solid rounded-full flex justify-center items-center text-xs">
-                    <i class="fa-sharp fa-solid fa-xmark"></i>
-                </div>
-            </div>`;
-        DOM.tag.insertAdjacentHTML("beforeend", obj);
-        e.target.style.display = "none";
-        let tag = {
-          name: e.target.textContent,
-          type: "appareil",
-          action: "Add",
-        };
-        AddOrRemoveTag(tag);
-      } else if (e.target.closest("#ustensiles")) {
-        let obj = `<div data='${e.target.getAttribute(
-          "value"
-        )}' class="bg-customOrange rounded-sm py-2 px-3 text-white mr-2 text-sm flex items-center justify-center cursor-pointer">
-                <div>${e.target.textContent}</div>
-                <div class="deleteTags ml-2 h-5 w-5 border border-white border-solid rounded-full flex justify-center items-center text-xs">
-                    <i class="fa-sharp fa-solid fa-xmark"></i>
-                </div>
-            </div>`;
-        DOM.tag.insertAdjacentHTML("beforeend", obj);
-        e.target.style.display = "none";
-        let tag = {
-          name: e.target.textContent,
-          type: "ustensil",
-          action: "Add",
-        };
-        AddOrRemoveTag(tag);
-      }
-      addingListenerToTags();
-    });
-  });
-}
-addingListenerToItems();
-
-// Adding listener for deleting tags selected
-function addingListenerToTags() {
-  const tags = document.querySelectorAll("#tags>div>.deleteTags");
-  tags.forEach((tag) => {
-    tag.addEventListener("click", function (e) {
-      let data = e.target.parentElement.parentElement.getAttribute("data");
-      e.target.parentElement.parentElement.remove();
-      let item = document.querySelectorAll(`span[value='${data}']`);
-      /// IM HERE
-      item[0].style.display = "block";
-      let tag = { name: data, type: "appareil", action: "Remove" };
-      AddOrRemoveTag(tag);
-    });
-  });
 }
